e370374f341fd97f2d8e8000c8277a5fee22f200
kobatoha <kobatoha@gmail.com>
2024-11-29 09:18:19 +0300

    select run res a separate function

--- unicorn.py
+++ unicorn.py
@@ -1122,6 +1122,47 @@ class Main(Ui_MainWindow):
             self._res_thread = None
             self.label_res.setText("")
 
+    def run_res(self, hwnd):
+        redss.check_active_window(hwnd)
+        self.paused_pressed()
+        time.sleep(3)
+        rect_recovery_free_exp = redss.get_window_free_up(hwnd)
+        rect_recovery_agree = redss.get_window_free_agree(hwnd)
+        rect_recovery_pay_exp = redss.get_window_pay_agree(hwnd)
+        if self.pressed_res:
+            redss.send_left_click_pyautogui(hwnd, rect_recovery_free_exp[0], rect_recovery_free_exp[1])
+            time.sleep(0.5)
+            death = False
+            if self.pressed_res:
+                redss.send_left_click_pyautogui(hwnd, rect_recovery_agree[0], rect_recovery_agree[1])
+                time.sleep(0.5)
+                death = False
+                if self.pressed_res:
+                    redss.send_left_click_pyautogui(
+                        hwnd,
+                        rect_recovery_pay_exp[0],
+                        rect_recovery_pay_exp[1]
+                    )
+                    time.sleep(0.5)
+                    death = True
+        else:
+            death = False
+            self.res_process = False
+            self.continue_pressed()
+
+        if death:
+            self.paused_pressed()
+            time.sleep(100)
+            if self.pressed_res:
+                redss.use_teleport(hwnd)
+                time.sleep(2)
+                self.continue_pressed()
+                self.res_process = False
+            else:
+                self.res_process = False
+
+        return death
+
     def press_res(self):
         time.sleep(3)
         while self.pressed_res:
@@ -1131,45 +1172,7 @@ class Main(Ui_MainWindow):
                 if flag:
                     self.res_process = True
                 if flag and self.pressed_res:
-                    redss.check_active_window(hwnd)
-                    self.paused_pressed()
-                    time.sleep(3)
-                    rect_recovery_free_exp = redss.get_window_free_up(hwnd)
-                    rect_recovery_agree = redss.get_window_free_agree(hwnd)
-                    rect_recovery_pay_exp = redss.get_window_pay_agree(hwnd)
-                    if self.pressed_res:
-                        redss.send_left_click_pyautogui(hwnd, rect_recovery_free_exp[0], rect_recovery_free_exp[1])
-                        time.sleep(0.5)
-                        death = False
-                        if self.pressed_res:
-                            redss.send_left_click_pyautogui(hwnd, rect_recovery_agree[0], rect_recovery_agree[1])
-                            time.sleep(0.5)
-                            death = False
-                            if self.pressed_res:
-                                redss.send_left_click_pyautogui(
-                                    hwnd,
-                                    rect_recovery_pay_exp[0],
-                                    rect_recovery_pay_exp[1]
-                                )
-                                time.sleep(0.5)
-                                death = True
-                    else:
-                        break
-                else:
-                    death = False
-                    self.res_process = False
-                    self.continue_pressed()
-                if death:
-                    self.paused_pressed()
-                    time.sleep(100)
-                    if self.pressed_res:
-                        redss.use_teleport(hwnd)
-                        time.sleep(2)
-                        self.continue_pressed()
-                        self.res_process = False
-                    else:
-                        self.res_process = False
-                        break
+                        death = self.run_res(hwnd=hwnd)
 
                 respawn = random.randint(30000, 40000)
                 total_seconds = respawn / 1000
