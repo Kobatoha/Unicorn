66816e5aa7261fde0d9caea9755fa73f08ab0ae6
kobatoha <kobatoha@gmail.com>
2024-12-09 23:37:27 +0300

    init base ui code

---
+++ ui/base_ui.py
@@ -0,0 +1,18 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class BaseUi:
+    def __init__(self):
+        self.central_widget = None
+
+    def setup_ui(self, main_window):
+        main_window.setObjectName("main_window")
+        main_window.setWindowTitle("Unicorn")
+        main_window.setWindowIcon(QtGui.QIcon("resources/images/unicorn.png"))
+        main_window.resize(380, 600)
+        main_window.setMinimumSize(QtCore.QSize(165, 380))
+        main_window.setMaximumSize(QtCore.QSize(380, 600))
+        main_window.setStyleSheet("background-image: url('resources/images/bg_image.jpg');")
+
+        self.central_widget = QtWidgets.QWidget(main_window)
+        self.central_widget.setObjectName("centralwidget")
