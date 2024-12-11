e474e9ceb23b166a44a673a6c2cffec9d2af04f7
kobatoha <kobatoha@gmail.com>
2024-12-11 19:15:50 +0300

    init states

---
+++ logic/state_manager.py
@@ -0,0 +1,65 @@
+class AppState:
+    def __init__(self):
+
+        self.pressed_f1 = False
+        self._f1_thread = None
+
+        self.pressed_f2 = False
+        self._f2_thread = None
+
+        self.pressed_f3 = False
+        self._f3_thread = None
+
+        self.pressed_f4 = False
+        self._f4_thread = None
+
+        self.pressed_f5 = False
+        self._f5_thread = None
+
+        self.pressed_Q = False
+        self._Q_thread = None
+
+        self.pressed_W = False
+        self._W_thread = None
+
+        self.pressed_E = False
+        self._E_thread = None
+
+        self.pressed_R = False
+        self._R_thread = None
+
+        self.pressed_T = False
+        self._T_thread = None
+
+        self.pressed_1 = False
+        self._1_thread = None
+
+        self.pressed_2 = False
+        self._2_thread = None
+
+        self.pressed_3 = False
+        self._3_thread = None
+
+        self.pressed_4 = False
+        self._4_thread = None
+
+        self.pressed_5 = False
+        self._5_thread = None
+
+        self.pressed_6 = False
+        self._6_thread = None
+
+        self.pressed_7 = False
+        self._7_thread = None
+
+        self.pressed_8 = False
+        self._8_thread = None
+
+        self.pressed_9 = False
+        self._9_thread = None
+
+        self.pressed_tilda = False
+        self._tilda_thread = None
+
+
+app_state = AppState()
