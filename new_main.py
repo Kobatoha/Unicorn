cb602912f3c54cd1558e176898649f5a68ea4633
kobatoha <kobatoha@gmail.com>
2024-12-22 12:18:41 +0300

    add interface widgets in central widget

--- new_main.py
+++ new_main.py
@@ -4,19 +4,24 @@ from ui.base_ui import BaseUI
 from ui.system_ui import SystemUI
 from ui.resurrection_ui import ResurrectionUI
 from ui.keyboard_ui import KeyboardUI
+from ui.interface_ui import InterfaceUI
 
 
-class MainApp(BaseUI, SystemUI, ResurrectionUI, KeyboardUI):
+class MainApp(BaseUI, SystemUI, ResurrectionUI, KeyboardUI, InterfaceUI):
     def __init__(self):
         super().__init__()
         self.MainWindow = QtWidgets.QMainWindow()
         self.setup_ui(self.MainWindow)
-        # self.add_system_buttons(self.central_widget)
-        self.add_resurrection_widgets(self.central_widget)
-        self.add_keyboard_left_line_edits(self.central_widget)
-        self.add_keyboard_left_check_boxes(self.central_widget)
-        self.add_keyboard_right_line_edits(self.central_widget)
-        self.add_keyboard_right_check_boxes(self.central_widget)
+        self.add_widgets(self.central_widget)
+
+    def add_widgets(self, parent):
+        self.add_resurrection_widgets(parent)
+        self.add_keyboard_left_line_edits(parent)
+        self.add_keyboard_left_check_boxes(parent)
+        self.add_keyboard_right_line_edits(parent)
+        self.add_keyboard_right_check_boxes(parent)
+        self.add_interface_widgets(parent)
+        self.add_hottime_widgets(parent)
 
     def run(self):
         self.MainWindow.show()
