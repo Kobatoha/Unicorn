e353e7773ded24652cff0bf3ef16b17e8107e280
Alexandra <126861307+Kobatoha@users.noreply.github.com>
2024-12-29 22:23:11 +0300

    optimization system ui

--- ui/system_ui.py
+++ ui/system_ui.py
@@ -1,77 +1,104 @@
-from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtWidgets import QPushButton, QLabel, QSystemTrayIcon, QLineEdit
+from PyQt5.QtGui import QFont, QIcon
+from PyQt5.QtCore import QSize, Qt
 
 
 class SystemUI:
     def __init__(self):
-        self.stop_button = None
-        self.start_button = None
+        self.system_line_edits = None
+        self.msg_box_active = False
+        self.tray_icon = None
+        self.system_buttons = None
+        self.system_labels = None
 
     def create_system_buttons(self, parent):
 
-        self.pushButton_startstop = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_startstop.setGeometry(QtCore.QRect(100, 490, 40, 40))
-        self.pushButton_startstop.setStyleSheet("background-color: rgb(255, 239, 220);")
-        font = QtGui.QFont()
-        font.setPointSize(10)
-        font.setBold(True)
-        font.setWeight(75)
-        self.pushButton_startstop.setFont(font)
-        self.pushButton_startstop.setObjectName("pushButton_startstop")
-        self.pushButton_startstop.setText('Start')
-        self.pushButton_startstop.clicked.connect(self.startstop)
+        button_font = QFont()
+        button_font.setPointSize(10)
+        button_font.setBold(True)
+        button_font.setWeight(75)
 
-        self.label_hotkey_startstop = QtWidgets.QLabel(self.centralwidget)
-        self.label_hotkey_startstop.setGeometry(QtCore.QRect(95, 530, 50, 21))
-        font = QtGui.QFont()
+        system_buttons = {
+            'startstop': {
+                'object_name': 'startstop',
+                'rect': [100, 490, 40, 40],
+                'text': 'Start',
+                'icon': '',
+                'icon_size': None
+            },
+            'locate': {
+                'object_name': 'locate',
+                'rect': [30, 490, 41, 41],
+                'text': '',
+                'icon': 'resources/images/target_off.png',
+                'icon_size': [40, 40]
+            }
+        }
+
+        self.system_buttons = {}
+
+        for key, properties in system_buttons.items():
+            button = QPushButton(parent)
+            rect = properties['rect']
+            button.setGeometry(rect[0], rect[1], rect[2], rect[3])
+            button.setFont(button_font)
+            button.setObjectName(properties['object_name'])
+            button.setText(properties['text'])
+            if properties['icon'] and properties['icon_size']:
+                button.setIcon(QIcon(properties['icon']))
+                button.setIconSize(QSize(properties['icon_size'][0], properties['icon_size'][1]))
+            button.setStyleSheet("background-color: rgb(255, 239, 220);")
+
+            self.system_buttons[key] = button
+
+    def create_system_widgets(self, parent):
+
+        font = QFont()
         font.setPointSize(7)
         font.setItalic(False)
         font.setUnderline(False)
         font.setStrikeOut(False)
         font.setKerning(True)
-        self.label_hotkey_startstop.setFont(font)
-        self.label_hotkey_startstop.setAlignment(QtCore.Qt.AlignCenter)
-        self.label_hotkey_startstop.setObjectName("label_hotkey_startstop")
-        self.label_hotkey_startstop.setText('INSERT')
-        self.label_hotkey_startstop.setStyleSheet("background: none;")
 
-        self.tray_icon = QtWidgets.QSystemTrayIcon()
-        self.tray_icon.setIcon(QtGui.QIcon("images/icon_off.png"))
+        system_labels = {
+            'label_startstop': {'object_name': 'label_startstop', 'rect': [102, 530, 50, 21], 'text': 'INSERT'},
+            'label_hwnd': {'object_name': 'label_hwnd', 'rect': [25, 530, 50, 21], 'text': ''},
+        }
 
-        # Отображаем иконку
+        self.system_labels = {}
+
+        for key, properties in system_labels.items():
+            label = QLabel(parent)
+            rect = properties['rect']
+            label.setGeometry(rect[0], rect[1], rect[2], rect[3])
+            label.setObjectName(properties['object_name'])
+            label.setText(properties['text'])
+            label.setFont(font)
+            label.setStyleSheet("background: none;")
+
+            self.system_labels[key] = label
+
+        self.tray_icon = QSystemTrayIcon()
+        self.tray_icon.setIcon(QIcon("resources/images/icon_off.png"))
+
         self.tray_icon.show()
 
-        ### button lockated ###
-        self.pushButton_located = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_located.setGeometry(QtCore.QRect(30, 490, 41, 41))
-        self.pushButton_located.setStyleSheet("background-color: rgb(255, 239, 220);")
-        font = QtGui.QFont()
-        font.setPointSize(8)
-        font.setBold(False)
-        font.setWeight(50)
-        self.pushButton_located.setFont(font)
-        self.pushButton_located.setObjectName("pushButton_located")
-        self.pushButton_located.setIcon(QtGui.QIcon('images/target_off.png'))
-        self.pushButton_located.setIconSize(QtCore.QSize(40, 40))
-        self.pushButton_located.clicked.connect(self.get_window_id)
+        system_line_edits = {
+            'line_edit_hwnd': {'object_name': 'line_edit_profile_1', 'rect': [40, 460, 90, 20]},
+        }
 
-        self.msg_box_active = False
+        self.system_line_edits = {}
 
-        self.label_id_window = QtWidgets.QLabel(self.centralwidget)
-        self.label_id_window.setGeometry(QtCore.QRect(25, 530, 50, 21))
-        font = QtGui.QFont()
-        font.setPointSize(7)
-        font.setItalic(False)
-        font.setUnderline(False)
-        font.setStrikeOut(False)
-        font.setKerning(True)
-        self.label_id_window.setFont(font)
-        self.label_id_window.setAlignment(QtCore.Qt.AlignCenter)
-        self.label_id_window.setObjectName("label_id_window")
-        self.label_id_window.setText("")
-        self.label_id_window.setStyleSheet("background: none;")
+        for key, properties in system_line_edits.items():
+            line_edit = QLineEdit(parent)
+            rect = properties['rect']
+            line_edit.setGeometry(rect[0], rect[1], rect[2], rect[3])
+            line_edit.setObjectName(properties['object_name'])
+            line_edit.setStyleSheet("background-color: rgb(239, 239, 239);")
+            line_edit.setAlignment(Qt.AlignCenter | Qt.AlignHCenter)
 
-        self.lineEdit_window_id = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit_window_id.setGeometry(QtCore.QRect(40, 460, 90, 20))
-        self.lineEdit_window_id.setObjectName("lineEdit_window_id")
-        self.lineEdit_window_id.setStyleSheet("background-color: rgb(239, 239, 239);")
-        self.lineEdit_window_id.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignHCenter)
+            self.system_line_edits[key] = line_edit
+
+    def add_system_widgets(self, parent):
+        self.create_system_buttons(parent)
+        self.create_system_widgets(parent)
