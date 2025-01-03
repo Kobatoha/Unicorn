a35c259edf19308fb00abe4be7624518e2d7a82e
Alexandra <126861307+Kobatoha@users.noreply.github.com>
2025-01-03 23:51:00 +0300

    optimization resurrection widgets

--- ui/resurrection_ui.py
+++ ui/resurrection_ui.py
@@ -6,65 +6,88 @@ from logic.state_manager import *
 
 class ResurrectionUI:
     def __init__(self):
-        self.check_box_res_random_without_wait = None
-        self.check_box_res_random = None
-        self.check_box_res_without_wait = None
-        self.check_box_res = None
-        self.label_night_teleport_solo = None
-        self.label_night_teleport = None
-        self.label_res_random = None
-        self.label_res = None
+        self.resurrection_labels = None
+        self.resurrection_check_boxes = None
 
     def create_resurrection_widgets(self, parent):
-        self.label_res = QLabel(parent)
-        self.label_res.setGeometry(QRect(180, 320, 60, 20))
-        self.label_res.setObjectName("lineEdit_res")
-        self.label_res.setStyleSheet("background: none;")
+        font = QFont()
+        font.setPointSize(7)
+        font.setItalic(False)
+        font.setUnderline(False)
+        font.setStrikeOut(False)
+        font.setKerning(True)
 
-        self.label_res_random = QLabel(parent)
-        self.label_res_random.setGeometry(QRect(180, 350, 60, 20))
-        self.label_res_random.setObjectName("lineEdit_res_random")
-        self.label_res_random.setStyleSheet("background: none;")
+        resurrection_labels = {
+            'res': {'object_name': 'label_res', 'rect': [180, 320, 60, 20], 'text': ''},
+            'res_random': {'object_name': 'label_res_random', 'rect': [180, 350, 60, 20], 'text': ''},
+            'night_teleport': {'object_name': 'label_night_teleport', 'rect': [30, 350, 60, 20], 'text': ''},
+            'night_teleport_solo': {'object_name': 'label_night_teleport_solo', 'rect': [30, 320, 60, 20], 'text': ''},
+        }
 
-        self.label_night_teleport = QLabel(parent)
-        self.label_night_teleport.setGeometry(QRect(30, 350, 60, 20))
-        self.label_night_teleport.setObjectName("label_night_teleport")
-        self.label_night_teleport.setStyleSheet("background: none;")
+        self.resurrection_labels = {}
 
-        self.label_night_teleport_solo = QLabel(parent)
-        self.label_night_teleport_solo.setGeometry(QRect(30, 320, 60, 20))
-        self.label_night_teleport_solo.setObjectName("label_night_teleport")
-        self.label_night_teleport_solo.setStyleSheet("background: none;")
+        for key, properties in resurrection_labels.items():
+            label = QLabel(parent)
+            rect = properties['rect']
+            label.setGeometry(rect[0], rect[1], rect[2], rect[3])
+            label.setObjectName(properties['object_name'])
+            label.setText(properties['text'])
+            label.setFont(font)
+            label.setStyleSheet("background: none;")
+
+            self.resurrection_labels[key] = label
 
     def create_resurrection_check_boxes(self, parent):
-        font = QFont()
-        font.setBold(True)
-        font.setWeight(75)
-        
-        self.check_box_res = QCheckBox(parent)
-        self.check_box_res.setGeometry(QRect(250, 320, 40, 20)) 
-        self.check_box_res.setFont(font)
-        self.check_box_res.setObjectName("check_box_res")
-        self.check_box_res.setText("res")
-        self.check_box_res.setStyleSheet("background: none;")
-        self.check_box_res.setToolTip('Свободный телепорт должен быть на F11')
+        check_box_font = QFont()
+        check_box_font.setBold(True)
+        check_box_font.setWeight(75)
 
-        self.check_box_res_without_wait = QCheckBox(parent)
-        self.check_box_res_without_wait.setGeometry(QRect(350, 320, 20, 20))
-        self.check_box_res_without_wait.setStyleSheet("background: none;")
-        self.check_box_res_without_wait.setDisabled(True)
-        self.check_box_res_without_wait.setToolTip('Перезалет по точке через 5 секунд после реса')
+        resurrection_check_boxes = {
+            'res': {
+                'object_name': 'check_box_res', 
+                'rect': [250, 320, 40, 20], 
+                'text': 'res', 
+                'tool_tip': 'Свободный телепорт должен быть на F11',
+                'disabled': False
+            },
+            'res_without_wait': {
+                'object_name': 'check_box_res_without_wait',
+                'rect': [350, 320, 20, 20],
+                'text': '',
+                'tool_tip': 'Перезалет по точке через 5 секунд после реса',
+                'disabled': True
+            },
+            'res_random': {
+                'object_name': 'check_box_res_random',
+                'rect': [250, 350, 85, 20],
+                'text': 'res random',
+                'tool_tip': 'Свободный телепорт должен быть на F7, F8, F9, F10, F11',
+                'disabled': False
+            },
+            'res_random_without_wait': {
+                'object_name': 'check_box_res_random_without_wait',
+                'rect': [350, 350, 20, 20],
+                'text': 'F4',
+                'tool_tip': 'Перезалет по точке через 5 секунд после реса',
+                'disabled': True
+            }
+        }
 
-        self.check_box_res_random = QCheckBox(parent)
-        self.check_box_res_random.setGeometry(QRect(250, 350, 85, 20))
-        self.check_box_res_random.setFont(font)
-        self.check_box_res_random.setObjectName("check_box_res_random")
-        self.check_box_res_random.setText("res random")
-        self.check_box_res_random.setStyleSheet("background: none;")
-        self.check_box_res_random.setToolTip('Свободный телепорт должен быть на F7, F8, F9, F10, F11')
+        self.resurrection_check_boxes = {}
 
-        self.check_box_res_random_without_wait = QCheckBox(parent)
-        self.check_box_res_random_without_wait.setGeometry(QRect(350, 350, 20, 20))
-        self.check_box_res_random_without_wait.setStyleSheet("background: none;")
-        self.check_box_res_random_without_wait.setDisabled(True)
-        self.check_box_res_random_without_wait.setToolTip('Перезалет по точке через 5 секунд после реса')
+        for key, properties in resurrection_check_boxes.items():
+            check_box = QCheckBox(parent)
+            rect = properties['rect']
+            check_box.setGeometry(rect[0], rect[1], rect[2], rect[3])
+            check_box.setFont(check_box_font)
+            check_box.setObjectName(properties['object_name'])
+            check_box.setText(properties['text'])
+            check_box.setStyleSheet("background: none;")
+            check_box.setDisabled(properties['disabled'])
+            check_box.setToolTip(properties['tool_tip'])
+
+            self.resurrection_check_boxes[key] = check_box
+
+    def add_resurrection_widgets(self, parent):
+        self.create_resurrection_check_boxes(parent)
+        self.create_resurrection_widgets(parent)
